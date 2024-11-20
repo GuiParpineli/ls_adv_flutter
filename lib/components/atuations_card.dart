7708feb48a853983e06f42ca22ddd4ce799ebaf1
guiparpineli <guiparpineli@hotmail.com>
2024-11-20 19:04:02 -0300

    feat: add responsabisive layout

--- lib/components/atuations_card.dart
+++ lib/components/atuations_card.dart
@@ -10,69 +10,127 @@ class AtuationsCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
-      child: Flex(
-        direction: Axis.vertical,
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            color: Theme.of(context).cardTheme.color!.withOpacity(0.3),
-            shadowColor: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 450,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(26),
-                    ),
-                    child: Image(
-                      image: AssetImage(atuation.image),
-                      fit: BoxFit.cover,
-                    ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            // Layout para telas pequenas
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Text(
-                                  atuation.title,
-                                  style: const TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
+                  color: Theme.of(context).cardTheme.color!.withOpacity(0.3),
+                  shadowColor: Colors.black,
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          clipBehavior: Clip.hardEdge,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(26),
+                          ),
+                          child: Image(
+                            image: AssetImage(atuation.image),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                atuation.title,
+                                style: const TextStyle(
+                                    fontSize: 24, fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 18.0),
+                                child: Text(
+                                  atuation.description,
+                                  softWrap: true,
+                                  textAlign: TextAlign.center,
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(top: 18.0, left: 8.0),
-                            child: Text(
-                              atuation.description,
-                              softWrap: true,
-                              textAlign: TextAlign.end,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
+              ],
+            );
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                color: Theme.of(context).cardTheme.color!.withOpacity(0.3),
+                shadowColor: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 450,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(26),
+                        ),
+                        child: Image(
+                          image: AssetImage(atuation.image),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      atuation.title,
+                                      style: const TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 18.0, left: 8.0),
+                                child: Text(
+                                  atuation.description,
+                                  softWrap: true,
+                                  textAlign: TextAlign.end,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
+            );
+          }
+        },
       ),
     );
   }
--- lib/components/lawyer_card.dart
+++ lib/components/lawyer_card.dart
@@ -11,18 +11,98 @@ class LawyerCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
-      child: Flex(
-        direction: Axis.vertical,
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
+      child: LayoutBuilder(
+        builder: (context, contraints) {
+          if (contraints.maxWidth < 600) {
+            return _buildMobileLayout(context);
+          } else {
+            return _buildDesktopLayout(context);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildDesktopLayout(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          color: Theme.of(context).cardTheme.color!.withOpacity(0.3),
+          shadowColor: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(26),
+                  ),
+                  child: Image(
+                    image: AssetImage(team.image),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              team.name,
+                              style: const TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              team.role,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 18.0, left: 8.0),
+                        child: Text(
+                          team.description,
+                          softWrap: true,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            color: Theme.of(context).cardTheme.color!.withOpacity(0.3),
-            shadowColor: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Row(
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMobileLayout(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          color: Theme.of(context).cardTheme.color!.withOpacity(0.3),
+          shadowColor: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Expanded(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Container(
@@ -36,44 +116,37 @@ class LawyerCard extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
-                  Expanded(
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                team.name,
-                                style: const TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                team.role,
-                                style: const TextStyle(fontSize: 14),
-                              ),
-                            ],
-                          ),
+                        Text(
+                          team.name,
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 18.0, left: 8.0),
-                          child: Text(
-                            team.description,
-                            softWrap: true,
-                            textAlign: TextAlign.end,
-                          ),
+                        Text(
+                          team.role,
+                          style: const TextStyle(fontSize: 14),
                         ),
                       ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 18.0, left: 8.0),
+                    child: Text(
+                      team.description,
+                      softWrap: true,
+                      textAlign: TextAlign.end,
                     ),
                   ),
                 ],
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
